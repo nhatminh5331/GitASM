383df996529c8a7fdf0d9c34cfef37e2b6481918
= <=>
2020-06-19 16:35:10 +0700

    update lab2

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -42,10 +42,10 @@ echo '<p>Students information:</p>';
 <table class="table table-bordered table-condensed">
     <thead>
       <tr>
-        <th>Student ID</th>
-        <th>Name</th>
-        <th>email</th>
-        <th>Class</th>
+        <th>Ma khach hang</th>
+        <th>Ten khach hang</th>
+        <th>sdt khach hang</th>
+        <th>Dia chi khach hang</th>
       </tr>
     </thead>
     <tbody>
@@ -56,10 +56,110 @@ echo '<p>Students information:</p>';
       ?>
    
       <tr>
-        <td scope="row"><?php echo $row['stuid'] ?></td>
-        <td><?php echo $row['fname'] ?></td>
-        <td><?php echo $row['email'] ?></td>
-        <td><?php echo $row['classname'] ?></td>
+        <td scope="row"><?php echo $row['makhachhang'] ?></td>
+        <td><?php echo $row['tenkhachhang'] ?></td>
+        <td><?php echo $row['sdtkhachhang'] ?></td>
+        <td><?php echo $row['diachikhachhang'] ?></td>
+        
+      </tr>
+     
+      <?php
+        }
+      ?>
+    </tbody>
+  </table>
+</div>
+
+<div id="container">
+<table class="table table-bordered table-condensed">
+    <thead>
+      <tr>
+        <th>Ma hoa don</th>
+        <th>Ngay hoa don</th>
+        <th>Ma nhan vien</th>
+        <th>Ma khach hang</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      // tạo vòng lặp 
+         //while($r = mysql_fetch_array($result)){
+             foreach ($resultSet as $row) {
+      ?>
+   
+      <tr>
+        <td scope="row"><?php echo $row['mahoadon'] ?></td>
+        <td><?php echo $row['ngayhoadon'] ?></td>
+        <td><?php echo $row['manhanvien'] ?></td>
+        <td><?php echo $row['makhachhang'] ?></td>
+        
+      </tr>
+     
+      <?php
+        }
+      ?>
+    </tbody>
+  </table>
+</div>
+
+<div id="container">
+<table class="table table-bordered table-condensed">
+    <thead>
+      <tr>
+        <th>Ma hoa don</th>
+        <th>Ma san pham</th>
+        <th>So luong</th>
+        <th>Gia san pham</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      // tạo vòng lặp 
+         //while($r = mysql_fetch_array($result)){
+             foreach ($resultSet as $row) {
+      ?>
+   
+      <tr>
+        <td scope="row"><?php echo $row['mahoadon'] ?></td>
+        <td><?php echo $row['masanpham'] ?></td>
+        <td><?php echo $row['so luong'] ?></td>
+        <td><?php echo $row['gia san pham'] ?></td>
+        
+      </tr>
+     
+      <?php
+        }
+      ?>
+    </tbody>
+  </table>
+</div>
+
+<div id="container">
+<table class="table table-bordered table-condensed">
+    <thead>
+      <tr>
+        <th>Ma san pham</th>
+        <th>Ten san pham</th>
+        <th>Size</th>
+        <th>Gia san pham</th>
+        <th>Ma cung cap</th>
+        <th>So du</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      // tạo vòng lặp 
+         //while($r = mysql_fetch_array($result)){
+             foreach ($resultSet as $row) {
+      ?>
+   
+      <tr>
+        <td scope="row"><?php echo $row['masanpham'] ?></td>
+        <td><?php echo $row['tensanpham'] ?></td>
+        <td><?php echo $row['size'] ?></td>
+        <td><?php echo $row['gia san pham'] ?></td>
+        <td><?php echo $row['macungcap'] ?></td>
+        <td><?php echo $row['sodu'] ?></td>
         
       </tr>
      
