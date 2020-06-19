f4c9f92d5bcd718f10f4afb5ef03cc44c8dc56d6
= <=>
2020-06-19 15:58:12 +0700

    update lab

--- DatabaseIndex.php
+++ DatabaseIndex.php
@@ -1,7 +1,6 @@
 <html>
-    <a href="DatabaseIndex.php" target="_blank" >Work with database</a>
-     <br/> 
-    <a href="ConnectToDB.php" target="_blank" >Connect database</a>
+
+     <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
      <br/>
@@ -9,9 +8,8 @@
     <br/>
     <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
      <br/>
-     
-
+     <a href="uploadfile.php" target="_blank" >upload file</a> 
+    
 </body>
-
      
 </html>
